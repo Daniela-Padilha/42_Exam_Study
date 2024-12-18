ea192519d7a2e2f061fcf4912e6a9d473e45271e
Daniela Padilha <129836273+Daniela-Padilha@users.noreply.github.com>
2024-12-18 16:17:47 +0000

    Update repeat_alpha.c

--- Milestone2/level1/repeat_alpha.c
+++ Milestone2/level1/repeat_alpha.c
@@ -29,8 +29,38 @@ $
 $>*/
 
 #include <unistd.h>
+
 int  main(int ac, char **av)
 {
-  
-  write(1, "\n", 1);
-  return (0);
+	int i = 0;
+	int a;
+  	if (ac == 2)
+  	{
+		while (av[1][i])
+		{
+			if (av[1][i] >= 'a' && av[1][i] <= 'z')
+			{
+				a = av[1][i];
+				while (a > 97)
+				{
+					write(1, &av[1][i], 1);
+					a--;
+				}
+			}
+			if (av[1][i] >= 'A' && av[1][i] <= 'Z')
+			{
+				a = av[1][i];
+				while (a > 64)
+				{
+					write(1, &av[1][i], 1);
+					a--;
+				}
+			}
+			else
+				write(1, &av[1][i], 1);
+			i++;
+		}
+	}
+ 	write(1, "\n", 1);
+	return (0);
+}
