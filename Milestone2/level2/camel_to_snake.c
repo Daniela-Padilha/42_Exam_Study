2ff820d129ba3d8a0aa5605f934084a3524fea72
Daniela Padilha <129836273+Daniela-Padilha@users.noreply.github.com>
2025-01-02 23:09:58 +0000

    Update camel_to_snake.c

--- Milestone2/level2/camel_to_snake.c
+++ Milestone2/level2/camel_to_snake.c
@@ -21,43 +21,25 @@ $>./camel_to_snake | cat -e
 $ */
 
 #include <unistd.h>
-#include <stdlib.h>
 
 int	main(int ac, char **av)
 {
 	int i;
-	int	j;
-	char *str;
 
-	while (av[1][i])
-		{
-			if (av[1][i] >= 'A' && av[1][i] <= 'Z')
-				j++;
-			i++;
-		}
-	str =(char *)malloc((i * sizeof(char)) + j);
 	if (ac == 2)
 	{
 		i = 0;
-		j = 0;
 		while (av[1][i])
 		{
-			if (av[1][i] >= 'a' && av[1][i] <= 'z')
+			if (av[1][i] >= 'A' && av[1][i] <= 'Z')
 			{
-				str[j] = write(1, &str[j], 1);
+				av[1][i] = av[1][i] + 32;
+				write (1, "_", 1);
 			}
-			else if (av[1][i] >= 'A' && av[1][i] <= 'Z')
-			{
-				str[j] = '_';
-				j++;
-				str[j] = av[1][i] + 32;
-				write(1, &str[j], 1);
-			}
+			write (1, &av[1][i], 1);
 			i++;
 		}
 	}
 	write (1, "\n", 1);
 	return (0);
 }
-
-NOT FINISHED
