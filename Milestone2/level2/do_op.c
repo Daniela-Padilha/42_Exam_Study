18380f748259ca767d1a9a457030e60e0a73e8b8
Daniela Padilha <129836273+Daniela-Padilha@users.noreply.github.com>
2025-01-02 23:24:13 +0000

    Update do_op.c

--- Milestone2/level2/do_op.c
+++ Milestone2/level2/do_op.c
@@ -26,3 +26,25 @@ $> ./do_op "1" "+" "-43" | cat -e
 -42$
 $> ./do_op | cat -e
 $ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int	main(int ac, char **av)
+{
+	if (ac == 4)
+	{
+		if (av[2][0] == '*')
+			printf("%d", atoi(av[1]) * atoi(av[3]));
+		if (av[2][0] == '/')
+			printf("%d", atoi(av[1]) / atoi(av[3]));
+		if (av[2][0] == '+')
+			printf("%d", atoi(av[1]) + atoi(av[3]));
+		if (av[2][0] == '-')
+			printf("%d", atoi(av[1]) - atoi(av[3]));
+		if (av[2][0] == '%')
+			printf("%d", atoi(av[1]) % atoi(av[3]));
+	}
+	printf("\n");
+	return (0);
+}
