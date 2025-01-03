7dc575e5c584dda5cdeaadf0b7786d62975110a2
Daniela Padilha <129836273+Daniela-Padilha@users.noreply.github.com>
2025-01-03 15:13:54 +0000

    Update ft_strrev.c

---
+++ Milestone2/level2/ft_strrev.c
@@ -0,0 +1,36 @@
+/*Assignment name  : ft_strrev
+Expected files   : ft_strrev.c
+Allowed functions:
+--------------------------------------------------------------------------------
+
+Write a function that reverses (in-place) a string.
+
+It must return its parameter.
+
+Your function must be declared as follows:
+
+char    *ft_strrev(char *str);
+*/
+#include <stdio.h>
+
+char *ft_strrev(char *str)
+{
+	int i = 0;
+	int len = 0;
+	char temp;
+
+	if (!str)
+		return (NULL);
+	while (str[len])
+		len++;
+	len--;
+	while (i < len)
+	{
+		temp = str[i];
+		str[i] = str[len];
+		str[len] = temp;
+		i++;
+		len--;
+	}
+	return (str);
+}
