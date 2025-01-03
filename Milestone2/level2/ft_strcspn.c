7e3f68145e08e7aa124b8e44561459edc596ee95
Daniela Padilha <129836273+Daniela-Padilha@users.noreply.github.com>
2025-01-03 00:02:22 +0000

    Update ft_strcspn.c

--- Milestone2/level2/ft_strcspn.c
+++ Milestone2/level2/ft_strcspn.c
@@ -9,3 +9,24 @@ Reproduce exactly the behavior of the function strcspn
 The function should be prototyped as follows:
 
 size_t	ft_strcspn(const char *s, const char *reject); */
+
+#include <stddef.h>
+
+size_t ft_strcspn(const char *s, const char *reject)
+{
+	size_t i = 0;
+	size_t j = 0;
+	size_t count = 0;
+
+	while(s[i])
+	{
+		while (reject[j] && s[i] != reject[j])
+			j++;
+		if (reject[j] != '\0')
+			return (count);
+		j = 0;
+		count++;
+		i++;
+	}
+	return (count);
+}
