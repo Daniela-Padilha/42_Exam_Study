2e5ed5279bfbd5ff661351f7326ff914f891006d
Daniela Padilha <129836273+Daniela-Padilha@users.noreply.github.com>
2025-01-03 14:13:29 +0000

    Update ft_strdup.c

--- Milestone2/level2/ft_strdup.c
+++ Milestone2/level2/ft_strdup.c
@@ -9,3 +9,25 @@ Your function must be declared as follows:
 
 char    *ft_strdup(char *src);
  */
+#include <stdlib.h>
+
+char	*ft_strdup(char *src)
+{
+	int i = 0;
+	int len = 0;
+	char *s;
+
+	while (src[len])
+		len++;
+	s = malloc((len * sizeof(char)) + 1);
+	if (s != NULL)
+	{
+		while (src[i])
+		{
+			s[i] == src[i];
+			i++
+		}
+		s[i] = '\0';
+	}
+	return (s);
+}
