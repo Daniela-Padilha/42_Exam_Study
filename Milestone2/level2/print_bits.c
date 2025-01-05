5feb386b062f830dfcde8065917a9f1b9ef79705
Daniela Padilha <129836273+Daniela-Padilha@users.noreply.github.com>
2025-01-05 23:59:30 +0000

    Update print_bits.c

--- Milestone2/level2/print_bits.c
+++ Milestone2/level2/print_bits.c
@@ -11,3 +11,18 @@ Your function must be declared as follows:
 void	print_bits(unsigned char octet);
 
 Example, if you pass 2 to print_bits, it will print "00000010" */
+
+
+#include <unistd.h>
+void print_bits(unsigned char octect)
+{
+  int i = 8;
+  unsigned char bit;
+
+  while (i > 0)
+  {
+    bit = (octect >> i & 1) + '0';
+    write(1, &bit, 1);
+    i--;
+  }   
+}
