811f9109079c9928e1b8b88793399df82c6ccd58
Daniela Padilha <129836273+Daniela-Padilha@users.noreply.github.com>
2025-01-05 22:47:14 +0000

    Update ft_itoa.c

--- Milestone2/level4/ft_itoa.c
+++ Milestone2/level4/ft_itoa.c
@@ -9,3 +9,46 @@ The function returns the result in a char array that you must allocate.
 Your function must be declared as follows:
 
 char	*ft_itoa(int nbr);*/
+
+#include <stdlib.h>
+
+int ndigits(int nbr)
+{
+	int i = 0;
+
+	if (nbr == 0)
+		return (1);
+	if (nbr < 0)
+		i++;
+	while (nbr)
+	{
+		nbr = nbr /10;
+		i++;
+	}
+	return(i);
+}
+
+char *ft_itoa(int nbr)
+{
+	char *res;
+	int end = ndigits(nbr);
+
+	res = (char *)malloc(sizeof(char) * end + 1);
+	if (!res)
+		return(NULL);
+	if (nbr < 0)
+	{
+		nbr *= -1;
+		res[0] = '-';
+	}
+	if (nbr == 0)
+		res[0] = '0';
+	res[end] = '\0';
+	while (nbr)
+	{
+		res[end - 1] = (nbr % 10) + '0';
+		nbr = nbr / 10;
+		end--;
+	}
+	return (res);
+}
