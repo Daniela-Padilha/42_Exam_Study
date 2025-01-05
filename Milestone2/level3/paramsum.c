f22a5152c509dc63de9c083aa1884cb36a2423e3
Daniela Padilha <129836273+Daniela-Padilha@users.noreply.github.com>
2025-01-05 22:24:24 +0000

    Update paramsum.c

--- Milestone2/level3/paramsum.c
+++ Milestone2/level3/paramsum.c
@@ -17,3 +17,34 @@ $>./paramsum 6 12 24 | cat -e
 $>./paramsum | cat -e
 0$
 $>*/
+
+#include <unistd.h>
+
+void ft_putnbr(int nbr)
+{
+	char c;
+
+	if (nbr > 9)
+		 ft_putnbr(nbr / 10);
+	c = (nbr % 10) + '0';
+	write(1, &c, 1);
+}
+
+int main(int ac, char **av)
+{
+	int i = 1;
+
+	if (ac > 1)
+	{
+		while(ac > 1)
+		{
+			ac -= 1;
+			i++;
+		}
+	ft_putnbr(i - 1);
+	}
+	else if (ac == 1)
+		write(1, "0", 1);
+	write(1, "\n", 1);
+	return (0);
+}
